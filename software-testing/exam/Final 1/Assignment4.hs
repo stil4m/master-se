3d296d440416790791057685058b9e4312788af1
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-10-19 13:27:08 +0200

    Cleanup

--- software-testing/exam/Final 1/Assignment4.hs
+++
@@ -1,9 +0,0 @@
-module Assignment4
-
-
-where
-import Test.QuickCheck
-import LabExam
-
-testCreateTreeHuffman :: IO ()
-testCreateTreeHuffman = quickCheck (\s -> (not.null) s ==> (prop_huffman.createTree) s)
--- software-testing/exam/Final 1/Assignment5.hs
+++
@@ -1,8 +0,0 @@
-module Assignment5
-
-where
-import LabExam
-import Test.QuickCheck
-
-testBuildTreeHasHuffman :: IO ()
-testBuildTreeHasHuffman = quickCheck (\x -> (not.null) x ==> (prop_huffman.buildTree) x)
