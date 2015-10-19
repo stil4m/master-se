99dbe9116967ef0104defc77f7aa6d6af8178a27
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-10-19 10:37:59 +0200

    Added assignment 4

--- software-testing/exam/Assignment3.hs
+++ software-testing/exam/Assignment3.hs
@@ -4,7 +4,9 @@ where
 import Data.List
 import Test.QuickCheck
 
-freqList :: String -> [(Char,Int)]
+type FrequencyList = [(Char,Int)]
+
+freqList :: String -> FrequencyList
 freqList s = foldr mergeC [] (sort s) where
   mergeC a [] = [(a,1)]
   mergeC a (b@(c,n):bs) = if a == c then (c, n+1) : bs else (a,1) : b :bs
