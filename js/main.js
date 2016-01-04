d6ff422120558fd11f12f57ea4470e9eba2efe69
Mats Stijlaart <m.stijlaart@avisi.nl>
2016-01-04 13:14:45 +0100

    Master Preparation: Added master preparation page

--- js/main.js
+++ js/main.js
@@ -1,26 +1,46 @@
-
 angular.module('masterSE', ['ngRoute'])
-  .controller('MainCtrl', function($scope, $location, $timeout) {
+  .controller('MainCtrl', function ($scope, $location, $timeout, $interval ) {
+
+    var inter = null;
 
     $scope.data = {
-      template : null
+      template: null
     };
 
-    $scope.$on('$locationChangeStart', function(event) {
+    function loadTemplate(event) {
       if ($location.path() == null || $location.path().length == 0) {
         $scope.data.template = null;
         return
       }
-      $scope.data.template = "." + $location.path();
+      $scope.data.template = "." + $location.path() + ".html?" + new Date().getTime();
 
-      $timeout(function() {
-        MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+      $timeout(function () {
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
       }, 100);
-    });
+    }
+
+    function startInterval() {
+      $interval(function() {
+        $timeout(function(){
+          loadTemplate();
+        },1);
+
+      }, 1000);
+    }
+
+    if ($location.search().interval) {
+      startInterval();
+    }
+
+    $scope.$on('$locationChangeStart', loadTemplate);
   })
-  .directive('presentedItem', function() {
+  .directive('presentedItem', function () {
     return {
-      restrict : 'E',
-      template : '<span class="presented-by-lecturer">Presented by the lecturer.</span>'
+      restrict: 'E',
+      template: '<span class="presented-by-lecturer">Presented by the lecturer.</span>'
+    }
+  })
+  .directive('jsonTable', function() {
+    return {
     }
   });
